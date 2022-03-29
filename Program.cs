c21fb87cf5e4d52b14a9cdbffa37a9829ff28ebf
fhgaha <antsy1992@gmail.com>
2022-03-29 21:30:03 +0300

    from now on page number comes from web page

--- Program.cs
+++ Program.cs
@@ -15,29 +15,41 @@ namespace Learning_Web_Page_Parsing
     {
         static async Task Main(string[] args)
         {
-            var baseUrlString = "https://www.toy.ru/catalog/boy_transport/";
-            var additionUrlString = "?filterseccode%5B0%5D=transport&PAGEN_8=";
+            var baseUrl = "https://www.toy.ru/";
+            var categoryUrl = "catalog/boy_transport/";
+            var additionUrl = "?filterseccode%5B0%5D=transport&PAGEN_8=";
             var logPath = "products.csv";
 
-            if (string.IsNullOrWhiteSpace(File.ReadAllText(logPath)) )
+            if (string.IsNullOrWhiteSpace(File.ReadAllText(logPath)))
                 File.AppendAllText(logPath, "Название региона, Хлебные крошки, Название товара, Цена, Цена старая, " +
                     "Раздел с наличием, Ссылки на картинки, Ссылка на товар\n", Encoding.UTF8);
 
-            for (int i = 0; i < 11; i++)
+            IHtmlDocument doc_ = await ParsePage(baseUrl + categoryUrl);
+
+            int temp;
+            var pageAmount = doc_.QuerySelectorAll(@"a.page-link")
+                .Where(e => e.GetAttribute("href") != null)
+                .Where(e => e.GetAttribute("href").Contains("boy_transport"))
+                .Where(e => int.TryParse(e.TextContent, out temp))
+                .Select(e => int.Parse(e.TextContent))
+                .OrderBy(e => e)
+                .Last();
+
+            for (int i = 0; i < pageAmount; i++)
             {
-                string urlString = baseUrlString;
-                if (i > 1)
-                    urlString = baseUrlString + additionUrlString + i.ToString();
+                string urlString = baseUrl + categoryUrl;
+                if (i > 0)
+                    urlString = baseUrl + categoryUrl + additionUrl + i.ToString();
 
                 IHtmlDocument doc = await ParsePage(urlString);
 
-                if (doc is null) continue;
+                if (doc is null) break;
 
                 var cards = doc.GetElementsByClassName("h-100 product-card");
 
                 await foreach (var product in GenerateSequence(cards))
                 {
-                    Console.WriteLine($"Page {i}: " + product.Name);
+                    Console.WriteLine($"Page {i + 1}: " + product.Name);
 
                     var line = string.Join(',', new List<string>
                     {
@@ -55,6 +67,51 @@ namespace Learning_Web_Page_Parsing
                 }
             }
         }
+
+        #region sitemap doesn't contain "catalog/boy_transport/" or "boy_transport"
+        private static List<string> GetLinksFromSitemap(string baseUrl, string requiredSubString)
+        {
+            Console.WriteLine("Collecting links from sitemap...");
+
+            var doc = ParsePage(baseUrl + "/sitemap.xml").Result;
+            var currentLinks = doc.QuerySelectorAll("loc")
+                .Select(e => e.TextContent);
+
+            List<string> linksWithRequiredString = new();
+
+            foreach (var link in currentLinks)
+            {
+                if (link.Contains(requiredSubString))
+                    linksWithRequiredString.Add(link);
+            }
+
+            linksWithRequiredString.AddRange(FilterDecendantsLinks(currentLinks, requiredSubString));
+
+            Console.WriteLine("Links collected");
+            return linksWithRequiredString;
+        }
+
+        private static List<string> FilterDecendantsLinks(IEnumerable<string> baseLinks, string requiredSubString)
+        {
+            List<string> links = new();
+
+            foreach (var link in baseLinks)
+            {
+                if (link.Contains(requiredSubString))
+                    links.Add(link);
+
+                var doc = ParsePage(link).Result;
+                var currentLinks = doc.QuerySelectorAll("loc")
+                    .Select(e => e.TextContent);
+
+                if (currentLinks.Count() == 0) return links;
+
+                links.AddRange(FilterDecendantsLinks(currentLinks, requiredSubString));
+            }
+
+            return links;
+        }
+        #endregion
 
         public static async Task<IHtmlDocument> ParsePage(string urlString)
         {
