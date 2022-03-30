a36b267a6593b46852060331d9890d2ee424272e
fhgaha <antsy1992@gmail.com>
2022-03-30 13:45:52 +0300

    added the ability to parse information for the specified city

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using AngleSharp.Dom;
+﻿using AngleSharp;
+using AngleSharp.Dom;
 using AngleSharp.Html.Dom;
 using AngleSharp.Html.Parser;
 using System;
@@ -19,12 +20,14 @@ namespace Learning_Web_Page_Parsing
             var categoryUrl = "catalog/boy_transport/";
             var additionUrl = "?filterseccode%5B0%5D=transport&PAGEN_8=";
             var logPath = "products.csv";
+            //var requiredCity = "Санкт-Петербург";
+            var requiredCity = "Ростов-на-Дону";
 
             if (string.IsNullOrWhiteSpace(File.ReadAllText(logPath)))
                 File.AppendAllText(logPath, "Название региона, Хлебные крошки, Название товара, Цена, Цена старая, " +
                     "Раздел с наличием, Ссылки на картинки, Ссылка на товар\n", Encoding.UTF8);
 
-            IHtmlDocument doc_ = await ParsePage(baseUrl + categoryUrl);
+            IHtmlDocument doc_ = await ParsePage(baseUrl + categoryUrl, requiredCity);
 
             int temp;
             var pageAmount = doc_.QuerySelectorAll(@"a.page-link")
@@ -41,13 +44,13 @@ namespace Learning_Web_Page_Parsing
                 if (i > 0)
                     urlString = baseUrl + categoryUrl + additionUrl + i.ToString();
 
-                IHtmlDocument doc = await ParsePage(urlString);
+                IHtmlDocument doc = await ParsePage(urlString, requiredCity);
 
                 if (doc is null) break;
 
                 var cards = doc.GetElementsByClassName("h-100 product-card");
 
-                await foreach (var product in GenerateSequence(cards))
+                await foreach (var product in GenerateSequence(cards, requiredCity))
                 {
                     Console.WriteLine($"Page {i + 1}: " + product.Name);
 
@@ -68,12 +71,55 @@ namespace Learning_Web_Page_Parsing
             }
         }
 
+     
+
+        public static async Task<IHtmlDocument> ParsePage(string urlString, string requiredCity)
+        {
+            Dictionary<string, string> cityIdMap = new()
+            {
+                ["Санкт-Петербург"] = "78000000000",
+                ["Ростов-на-Дону"] = "61000001000"
+            };
+           
+            var doc = default(IHtmlDocument);
+            var client = new HttpClient();
+
+            client.DefaultRequestHeaders.Add("Cookie", "BITRIX_SM_city=" + cityIdMap[requiredCity]);
+
+            using (var stream = await client.GetStreamAsync(urlString))
+            {
+                var parser = new HtmlParser();
+                doc = await parser.ParseDocumentAsync(stream);
+            }
+
+            return doc;
+        }
+
+        public static async IAsyncEnumerable<Product> GenerateSequence(IHtmlCollection<IElement> cards, string requiredCity)
+        {
+            foreach (var card in cards)
+            {
+                var productUrl = "https://www.toy.ru" + card.QuerySelectorAll("a")
+                    .Single(m => m.GetAttribute("class") != null
+                        && m.GetAttribute("class") == "d-block p-1 product-name gtm-click")
+                    .GetAttribute("href");
+
+                var cardParsed = await ParsePage(productUrl, requiredCity);
+
+                if (cardParsed is null) continue;
+
+                var product = await Product.Get(cardParsed, productUrl);
+
+                yield return product;
+            };
+        }
+
         #region sitemap doesn't contain "catalog/boy_transport/" or "boy_transport"
-        private static List<string> GetLinksFromSitemap(string baseUrl, string requiredSubString)
+        private static List<string> GetLinksFromSitemap(string baseUrl, string requiredSubString, string requiredCity)
         {
             Console.WriteLine("Collecting links from sitemap...");
 
-            var doc = ParsePage(baseUrl + "/sitemap.xml").Result;
+            var doc = ParsePage(baseUrl + "/sitemap.xml", requiredCity).Result;
             var currentLinks = doc.QuerySelectorAll("loc")
                 .Select(e => e.TextContent);
 
@@ -85,13 +131,13 @@ namespace Learning_Web_Page_Parsing
                     linksWithRequiredString.Add(link);
             }
 
-            linksWithRequiredString.AddRange(FilterDecendantsLinks(currentLinks, requiredSubString));
+            linksWithRequiredString.AddRange(FilterDecendantsLinks(currentLinks, requiredSubString, requiredCity));
 
             Console.WriteLine("Links collected");
             return linksWithRequiredString;
         }
 
-        private static List<string> FilterDecendantsLinks(IEnumerable<string> baseLinks, string requiredSubString)
+        private static List<string> FilterDecendantsLinks(IEnumerable<string> baseLinks, string requiredSubString, string requiredCity)
         {
             List<string> links = new();
 
@@ -100,51 +146,18 @@ namespace Learning_Web_Page_Parsing
                 if (link.Contains(requiredSubString))
                     links.Add(link);
 
-                var doc = ParsePage(link).Result;
+                var doc = ParsePage(link, requiredCity).Result;
                 var currentLinks = doc.QuerySelectorAll("loc")
                     .Select(e => e.TextContent);
 
                 if (currentLinks.Count() == 0) return links;
 
-                links.AddRange(FilterDecendantsLinks(currentLinks, requiredSubString));
+                links.AddRange(FilterDecendantsLinks(currentLinks, requiredSubString, requiredCity));
             }
 
             return links;
         }
         #endregion
-
-        public static async Task<IHtmlDocument> ParsePage(string urlString)
-        {
-            var doc = default(IHtmlDocument);
-            var client = new HttpClient();
-
-            using (var stream = await client.GetStreamAsync(urlString))
-            {
-                var parser = new HtmlParser();
-                doc = await parser.ParseDocumentAsync(stream);
-            }
-
-            return doc;
-        }
-
-        public static async IAsyncEnumerable<Product> GenerateSequence(IHtmlCollection<IElement> cards)
-        {
-            foreach (var card in cards)
-            {
-                var productUrl = "https://www.toy.ru" + card.QuerySelectorAll("a")
-                    .Single(m => m.GetAttribute("class") != null
-                        && m.GetAttribute("class") == "d-block p-1 product-name gtm-click")
-                    .GetAttribute("href");
-
-                var cardParsed = await ParsePage(productUrl);
-
-                if (cardParsed is null) continue;
-
-                var product = await Product.Get(cardParsed, productUrl);
-
-                yield return product;
-            };
-        }
     }
 }
 
